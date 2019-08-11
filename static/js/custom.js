b0c0935bb0bf951ffe06f6486fbdbdb2dbd58d19
RakibCSE <rakibul.hossain365@gmail.com>
2019-08-12 01:31:39 +0600

    js added for ajax post request

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,5 @@
 
+//Get the location info when the page is ready.
 $(document).ready(function() {
     getLocation();
 
@@ -15,21 +16,27 @@ $(document).ready(function() {
     }
 });
 
-$("#help-me").click(function() {
-    $.ajax({
-        type: "POST",
-        url: "help-me/",
-        data: {
-            "csrfmiddlewaretoken": $("input[name=csrfmiddlewaretoken]").val(),
-            "loc_latitude": $("input[name=loc_latitude]").val(),
-            "loc_longitude": $("input[name=loc_longitude]").val()
-        },
-        success: function(data) {
-            document.getElementById("data-text").innerText = data;
-        },
-        error: function() {
-            alert("Error")
+//Ajax POST request to the URL
+$("#help-me").on("click", function(event) {
+    event.preventDefault();
+    let url = "/help-me/";
+    let data = {
+        csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+        loc_latitude: $("input[id=loc-latitude]").val(),
+        loc_longitude: $("input[id=loc-longitude").val(),
+    };
+    $.post(url, data, function (response) {
+        json_data = JSON.parse(response);
+        if (json_data.hasOwnProperty("formatted_address")) {
+            document.getElementById("help-text").innerHTML = "Sending emergency service to...";
+            document.getElementById("data-text").innerHTML = json_data["formatted_address"];
+            document.getElementById("coord-lat").innerHTML = "<b>Latitude: </b>" + parseFloat(json_data["latitude"]).toFixed(4);
+            document.getElementById("coord-lon").innerHTML = "<b>Longitude: </b>" + parseFloat(json_data["longitude"]).toFixed(4);
+        } else {
+            document.getElementById("help-text").innerHTML = "Something is wrong. Please try again."
+            document.getElementById("data-text").innerHTML = "";
+            document.getElementById("coord-lat").innerHTML = "";
+            document.getElementById("coord-lon").innerHTML = "";
         }
-    });
-});
-
+    })
+})
